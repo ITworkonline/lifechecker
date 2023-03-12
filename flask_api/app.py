93db0314d86ecd15f87f1b8ec2f04b75ed58b48e
itworkonline <jiwa0171@colorado.edu>
2023-03-12 16:18:14 -0600

    refine code with chatgpt

--- flask_api/app.py
+++ flask_api/app.py
@@ -1,16 +1,17 @@
 from flask import Flask, request, jsonify
 from datetime import datetime
+
 app = Flask(__name__)
 
 """
 Constant: Mock habits data
 Length of data: 3
-Attributes: name, createTime, frequency, threshold, completed
+Attributes: id, name, createTime, frequency, threshold, completed
 """
 habit_dict = {
-   1 : {"name": "Work out", "createTime": datetime.now(), "frequency": "weekly", "threshold":80, "completed":True},
-   2 : {"name": "Reading books", "createTime": datetime.now(), "frequency": "monthly", "threshold":50, "completed":True},
-   3: {"name": "Sports", "createTime": datetime.now(), "frequency": "monthly", "threshold":20, "completed":False},
+    1: {"id": 1, "name": "Work out", "createTime": datetime.now(), "frequency": "weekly", "threshold":80, "completed":True},
+    2: {"id": 2, "name": "Reading books", "createTime": datetime.now(), "frequency": "monthly", "threshold":50, "completed":True},
+    3: {"id": 3, "name": "Sports", "createTime": datetime.now(), "frequency": "monthly", "threshold":20, "completed":False},
 }
 
 """
@@ -34,80 +35,66 @@ def habits_route():
     if request.method == 'GET':
         return list_habits()
     elif request.method == "POST":
-        return create_habit(request.get_json(force=True))
+        return add_habit(request.get_json(force=True))
 
 """
 Concrete list of habits method
 """
 def list_habits():
-    return {"habits_list":habit_dict}
+    return {"habits_list":list(habit_dict.values())}
 
 """
-Concrete create new habit
+Concrete add new habit
 """
-@app.route('/addhabits', methods=['GET', 'POST'])
-def create_habit(new_habit):
-    habit_name = new_habit['name']
-    habit_dict[habit_name] = new_habit
-    return new_habit
+def add_habit(request_data):
+    habit_id = max(habit_dict.keys()) + 1
+    new_habit = {
+        "id": habit_id,
+        "name": request_data['name'],
+        "createTime": datetime.now(),
+        "frequency": request_data['frequency'],
+        "threshold": request_data['threshold'],
+        "completed": False
+    }
+    habit_dict[habit_id] = new_habit
+    return jsonify(new_habit)
 
 """
 Concrete update the habit based on id
 """
-@app.route('/update_habit/<int:id>', methods=['GET','PUT'])
-def update_todo_item(id):
-    # Get the request data
-    data = {
-            "name": "Writing the code", 
-            "createTime": datetime.now(), 
-            "frequency": "daily",
-            "threshold":50, 
-            "completed":True
-            }
-
-    # Check if todo item with the given ID exists
-    if id not in habit_dict:
-        return jsonify({"message": "Todo item not found"}), 404
-
-    # Update the todo item
-    habit_dict[id]["name"] = data["name"]
-
-    # Return the updated todo item
-    return jsonify({"habit_modified": habit_dict[id]}), 200
+@app.route('/habitslist/<int:habit_id>', methods=['GET','PUT', 'DELETE'])
+def habit_id_route(habit_id):
+    if request.method == 'GET':
+        return get_habit_attributes_by_id(habit_id)
+    elif request.method == "PUT":
+        return update_habit_by_id(habit_id, request.get_json(force=True))
+    elif request.method == "DELETE":
+        return delete_habit_by_id(habit_id)
 
 """
-API call based on GET, PUT, DELETE
-GET: Get habit's attributes
-PUT: Update the habit by name
-DELETE: Delete the habit by name
+Concrete get attributes by habit id
 """
-@app.route('/habitslist/<habit_name>', methods=['GET', 'PUT', 'DELETE'])
-def programming_language_route(habit_name):
-   if request.method == 'GET':
-       return get_habit_attributes(habit_name)
-   elif request.method == "PUT":
-       return update_habit(habit_name, request.get_json(force=True))
-   elif request.method == "DELETE":
-       return delete_programming_language(habit_name)
+def get_habit_attributes_by_id(habit_id):
+    if habit_id not in habit_dict:
+        return jsonify({"message": "Habit not found"}), 404
+    return habit_dict[habit_id]
 
 """
-Concrete get attributes by habit name
+Concrete update habit attributes by its id
 """
-def get_habit_attributes(habit_name):
-   return habit_dict[habit_name]
+def update_habit_by_id(habit_id, new_habit_attributes):
+    if habit_id not in habit_dict:
+        return jsonify({"message": "Habit not found"}), 404
+    habit = habit_dict[habit_id]
+    habit.update(new_habit_attributes)
+    return jsonify(habit)
 
 """
-Concrete update habit attributes by its name
+Concrete delete habit by its id
 """
-def update_habit(habit_name, new_habit_attributes):
-   habit = habit_dict[habit_name]
-   habit.update(new_habit_attributes)
-   return habit
-
-"""
-Concrete delete habit by its name
-"""
-def delete_programming_language(habit_name):
-   deleting_habit = habit_dict[habit_name]
-   del habit_dict[habit_name]
-   return deleting_habit
+def delete_habit_by_id(habit_id):
+    if habit_id not in habit_dict:
+        return jsonify({"message": "Habit not found"}), 404
+    deleting_habit = habit_dict[habit_id]
+    del habit_dict[habit_id]
+    return deleting_habit
